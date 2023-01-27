c4cf45ac2e481e65eb3a70e3caee128deb85039b
root <root@LAPTOP-P4D>
2023-01-26 22:17:40 -0600

    Updates

---
+++ precache-manifest.f2584a6b5a6da59019065330487be2d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e90508c8d99178ef91c4ecc3dac12a8",
+    "url": "/sheriff-of-lockwood/index.html"
+  },
+  {
+    "revision": "655d215406005518641d",
+    "url": "/sheriff-of-lockwood/static/css/main.0e96c88f.chunk.css"
+  },
+  {
+    "revision": "0c32e35eeea9980f0f3c",
+    "url": "/sheriff-of-lockwood/static/js/2.2680f6fb.chunk.js"
+  },
+  {
+    "revision": "0c649486e28c6e03e18a47439167ed05",
+    "url": "/sheriff-of-lockwood/static/js/2.2680f6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655d215406005518641d",
+    "url": "/sheriff-of-lockwood/static/js/main.37eeee06.chunk.js"
+  },
+  {
+    "revision": "6649e8abd2b5c70d1b73",
+    "url": "/sheriff-of-lockwood/static/js/runtime-main.8eafaf45.js"
+  }
+]);
