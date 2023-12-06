85c1529e0dea0bbeef6c9536edd3d34997ff90f2
Persona4dan <desu2dan@gmail.com>
2023-12-06 15:41:29 -0600

    Updates

--- precache-manifest.194197dc0cd457a414ca065323930a59.js
+++ precache-manifest.194197dc0cd457a414ca065323930a59.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "632c42cbb1b786cff946a67d51e63e03",
+    "revision": "44f7114dfd7de4591415ad0fc563fbb6",
     "url": "/sheriff-of-lockwood/index.html"
   },
   {
-    "revision": "5c5ff3e5670158ff7732",
+    "revision": "bbcbbedae4f1e57c998d",
     "url": "/sheriff-of-lockwood/static/css/main.0e96c88f.chunk.css"
   },
   {
-    "revision": "c166117db8d2928e0d3c",
-    "url": "/sheriff-of-lockwood/static/js/2.e3584140.chunk.js"
+    "revision": "1dc88e1f031a1e73da7f",
+    "url": "/sheriff-of-lockwood/static/js/2.a5ace6b8.chunk.js"
   },
   {
     "revision": "0c649486e28c6e03e18a47439167ed05",
-    "url": "/sheriff-of-lockwood/static/js/2.e3584140.chunk.js.LICENSE.txt"
+    "url": "/sheriff-of-lockwood/static/js/2.a5ace6b8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5c5ff3e5670158ff7732",
-    "url": "/sheriff-of-lockwood/static/js/main.895ad85a.chunk.js"
+    "revision": "bbcbbedae4f1e57c998d",
+    "url": "/sheriff-of-lockwood/static/js/main.a419626e.chunk.js"
   },
   {
     "revision": "6649e8abd2b5c70d1b73",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sheriff-of-lockwood/precache-manifest.315a6d658d2a34485ad0b51ecb5e3964.js"
+  "/sheriff-of-lockwood/precache-manifest.194197dc0cd457a414ca065323930a59.js"
 );
 
 self.addEventListener('message', (event) => {
